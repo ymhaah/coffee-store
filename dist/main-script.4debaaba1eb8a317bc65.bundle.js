10ebf26d28ec08b10bde3028ffdb18b42a302a46
youssef hafnawi <77534098+ymhaah@users.noreply.github.com>
2022-05-15 13:55:29 +0300

    finish the 4 cups
    cutting the background of the 4 cups for the hero-star

---
+++ dist/main-script.4debaaba1eb8a317bc65.bundle.js
@@ -0,0 +1,2 @@
+(()=>{console.log("Test");var e=document.querySelector(".nList"),n=document.querySelector("header .Container nav");e.onclick=function(){var o;o=n,e.classList.toggle("navList"),o.classList.toggle("exbend")},window.onscroll=function(){e.classList.remove("navList"),n.classList.remove("exbend")}})();
+//# sourceMappingURL=main-script.4debaaba1eb8a317bc65.bundle.js.map
